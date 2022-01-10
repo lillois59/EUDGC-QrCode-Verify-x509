889baf6296515e9233a640277f70aefd7a92df80
Camille Groult <lillois59119@hotmail.fr>
2022-01-10 17:45:43 +0100

    please mkdir+lib

---
+++ generator.js
@@ -0,0 +1,65 @@
+// Create a EUDGC Qr Code
+// https://ec.europa.eu/health/sites/default/files/ehealth/docs/digital-green-certificates_dt-specifications_en.pdf
+// Ref : https://blog.hqcodeshop.fi/archives/516-Decoding-EU-Digital-COVID-Certificate.html
+// Developer : camillegroult.fr
+
+const Generator = module.exports;
+const { encodeOne, Tagged } = require('cbor');
+const { deflate } = require('zlib');
+const { toFile } = require('qrcode');
+const { encode } = require('base45');
+
+const {
+    CLAIM_KEY_CERT,
+    CLAIM_KEY_V1EU,
+    CLAIM_ISSUER,
+    CLAIM_LAST_VAX_DATE,
+    CLAIM_QR_GEN_DATE,
+    CLAIM_HEADER_SIGN_ALGO,
+    CLAIM_HEADER_KEY_IDENTIFIER,
+    HEALTH_CERTIFICATE_PREFIX,
+    CBOR_TAG_VERSION
+} = require('./defs');
+
+Generator.pass2qr = (
+    algorithmSignature, keyIdentifier, hcertSignature, certIssuer,
+    vaccinationDate, qrCodeGeneratedDate /* opt */, payload, pathToQRimage
+) => new Promise((resolve, reject) => {
+
+    const qrCodeGenerationDate = qrCodeGeneratedDate || new Date().getTime();
+
+    const headerMap = new Map();
+    headerMap.set(CLAIM_HEADER_SIGN_ALGO, algorithmSignature);
+    headerMap.set(CLAIM_HEADER_KEY_IDENTIFIER, keyIdentifier);
+
+    const euPayloadMap = new Map();
+    euPayloadMap.set(CLAIM_KEY_V1EU, payload);
+
+    const masterMap = new Map();
+    masterMap.set(CLAIM_ISSUER, certIssuer); // eg: FI // for this exemple we develope remotly with friend
+    masterMap.set(CLAIM_LAST_VAX_DATE, vaccinationDate);
+    masterMap.set(CLAIM_QR_GEN_DATE, qrCodeGenerationDate);
+    masterMap.set(CLAIM_KEY_CERT, euPayloadMap);
+
+    const taggedObject = new Tagged(
+        CBOR_TAG_VERSION,
+        [
+            encodeOne(headerMap),
+            {}, // always empty? // need here some help Jari
+            encodeOne(masterMap),
+            hcertSignature
+        ],
+        null
+    );
+
+    const transportCWTdata = encodeOne(taggedObject);
+    deflate(transportCWTdata, async (err, deflCWTdata) => {
+        if (err) return reject(err);
+
+        const b45CWTdata = encode(deflCWTdata);
+        const qrReadyCWTstr = HEALTH_CERTIFICATE_PREFIX + b45CWTdata;
+
+        await toFile(pathToQRimage, qrReadyCWTstr);
+        resolve(qrReadyCWTstr);
+    });
+});
